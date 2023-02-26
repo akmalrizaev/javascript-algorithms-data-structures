33aa2a1823b31aa68a2e9653cfe510bde7e29f0e
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-02-26 08:38:49 +0500

    linear search algorithm

---
+++ Searching/linearSearch.js
@@ -0,0 +1,9 @@
+function linearSearch(arr, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+
+  return -1;
+}
+
+console.log(linearSearch([1, 23, 34, 5], 4));
