2177f39df99ca2a6567c5cdc20eaefac056e8dfc
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-02-27 18:08:37 +0500

    bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2, 5, 7, 45, -4, 8]));
