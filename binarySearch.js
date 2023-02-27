2de896f9f9bdb8f91a2dc2b8eacdfb3b4b150af0
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-02-27 11:33:08 +0500

    binary search

---
+++ binarySearch.js
@@ -0,0 +1,22 @@
+// Binary search is an efficient algorithm for finding an item from a sorted list of items
+
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 4, 5, 12, 15, 20, 25], 20));
