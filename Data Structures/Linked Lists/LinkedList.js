06408ed4032b0463f2b64f8771caf040737298ef
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-11-27 21:38:19 +0500

    Linked List - addToHead, addToTail, removeHead

--- Data Structures/Linked Lists/LinkedList.js
+++ Data Structures/Linked Lists/LinkedList.js
@@ -22,3 +22,12 @@ LinkedList.prototype.addToTail = function () {
   else this.head = newNode;
   this.tail = newNode;
 };
+
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+};
